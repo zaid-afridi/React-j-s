921f162dbaf8bb497635f32c4ef172885550d3e0
zaid-afridi <zaidafridi6868@gmail.com>
2026-03-03 11:08:33 +0500

    Add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 function App() {
   return (
     <>
-      <h1 className="bg-green-400 font-bold text-2xl">initionalized project</h1>
+      <div>
+        <h1 className="bg-green-400 font-bold text-2xl">
+          initionalized project
+        </h1>
+        <Fruit></Fruit>
+        <Color />
+      </div>
     </>
   );
 }
 
+function Fruit() {
+  return <h1 className="text-2xl bg-amber-200">Apple</h1>;
+}
+function Color() {
+  return <h1 className="text-2xl bg-amber-900">red</h1>;
+}
 export default App;
